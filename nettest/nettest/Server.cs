2c8074f059d45ee71c289322fa8e0324c1fe8f3a
unknown <Owen@.(none)>
2011-09-28 22:44:38 +0100

    initial commit

---
+++ nettest/nettest/Server.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace nettest
+{
+    public class Server
+    {
+        int portNumber;
+        UdpClient udpServer;
+        public Server(int port = 8024)
+        {
+            portNumber = port;
+            udpServer = new UdpClient(8024);
+        }
+        public void Update()
+        {
+            
+        }
+    }
+}
